b5dc573f490183ccf7c14f5c72555158d9d8d894
KKS <krystiankalinowski8@gmail.com>
2022-02-25 14:42:50 +0100

    Add Feature functionality

--- src/main/java/dk/bec/polonez/reservationsystem/controller/FeatureController.java
+++ src/main/java/dk/bec/polonez/reservationsystem/controller/FeatureController.java
@@ -1,4 +1,36 @@
 package dk.bec.polonez.reservationsystem.controller;
 
+import dk.bec.polonez.reservationsystem.dto.CreateFeatureDto;
+import dk.bec.polonez.reservationsystem.dto.ResponseFeatureDto;
+import dk.bec.polonez.reservationsystem.model.Feature;
+import dk.bec.polonez.reservationsystem.service.FeatureService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/feature/")
 public class FeatureController {
+    private final FeatureService featureService;
+
+    public FeatureController(FeatureService featureService){
+        this.featureService = featureService;
+    }
+
+    @GetMapping
+    public List<Feature> getAllFeatures() {
+        return featureService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Feature getFeatureById(@PathVariable long id) {
+        return featureService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseFeatureDto addFeature(@RequestBody CreateFeatureDto featureDto) {
+        return featureService.addFeature(featureDto);
+    }
 }
--- src/main/java/dk/bec/polonez/reservationsystem/service/FeatureService.java
+++ src/main/java/dk/bec/polonez/reservationsystem/service/FeatureService.java
@@ -1,4 +1,47 @@
 package dk.bec.polonez.reservationsystem.service;
 
+import dk.bec.polonez.reservationsystem.dto.CreateFeatureDto;
+import dk.bec.polonez.reservationsystem.dto.ResponseFeatureDto;
+import dk.bec.polonez.reservationsystem.model.Feature;
+import dk.bec.polonez.reservationsystem.repository.FeatureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FeatureService {
+
+    private final FeatureRepository featureRepository;
+
+    @Autowired
+    public FeatureService(FeatureRepository featureRepository) {
+        this.featureRepository = featureRepository;
+    }
+
+    public List<Feature> getAll() {
+        return featureRepository.findAll();
+    }
+
+    public Feature getById(long id) {
+        Optional<Feature> optionalFeature = featureRepository.findById(id);
+        return optionalFeature.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public ResponseFeatureDto addFeature(CreateFeatureDto featureDto) {
+        Feature.FeatureBuilder builder = Feature.builder();
+        Feature feature = builder
+                .name(featureDto.getName())
+                .description(featureDto.getDescription())
+                .build();
+        Feature savedFeature = featureRepository.save(feature);
+        ResponseFeatureDto.ResponseFeatureDtoBuilder response = ResponseFeatureDto.builder();
+        return response
+                .name(savedFeature.getName())
+                .description(savedFeature.getDescription())
+                .build();
+    }
 }
