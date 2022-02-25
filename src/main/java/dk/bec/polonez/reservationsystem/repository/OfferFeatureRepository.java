b43aef4cacfa7c19aba966e459a06d5ade0de6ce
KCA <kca@bec.dk>
2022-02-25 14:47:59 +0100

    Added repo and service for OfferFeature.

--- src/main/java/dk/bec/polonez/reservationsystem/repository/OfferFeatureRepository.java
+++ src/main/java/dk/bec/polonez/reservationsystem/repository/OfferFeatureRepository.java
@@ -1,4 +1,15 @@
 package dk.bec.polonez.reservationsystem.repository;
 
-public interface OfferFeatureRepository {
+import dk.bec.polonez.reservationsystem.model.OfferFeature;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface OfferFeatureRepository extends JpaRepository<OfferFeature, Long> {
+
+    Set<OfferFeature> findAllFeaturesForOffer(long OfferID);
+
+    Set<OfferFeature> findAllOffersUsingFeature(long FeatureID);
 }
