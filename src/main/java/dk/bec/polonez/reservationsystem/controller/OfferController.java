b6a42d769e7a5a5cbce009be40d6a30201fa4804
est <est@bec.dk>
2022-03-01 14:21:34 +0100

    Offer classes from Emil.

--- src/main/java/dk/bec/polonez/reservationsystem/controller/OfferController.java
+++ src/main/java/dk/bec/polonez/reservationsystem/controller/OfferController.java
@@ -1,16 +1,16 @@
 package dk.bec.polonez.reservationsystem.controller;
 
 import dk.bec.polonez.reservationsystem.dto.offerDto.CreateOfferDto;
-import dk.bec.polonez.reservationsystem.dto.offerDto.ResponseOfferDto;
+import dk.bec.polonez.reservationsystem.dto.offerDto.OfferDto;
+import dk.bec.polonez.reservationsystem.dto.offerDto.ResponseOfferFeatureDto;
 import dk.bec.polonez.reservationsystem.model.Offer;
 import dk.bec.polonez.reservationsystem.service.OfferService;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/offer/")
+@RequestMapping("/api/offers/")
 public class OfferController {
 
     private final OfferService offerService;
@@ -20,18 +20,32 @@ public class OfferController {
     }
 
     @GetMapping
-    public List<Offer> getAllOffers() {
+    public List<OfferDto> getAllOffers() {
         return offerService.getAll();
     }
 
     @GetMapping("{id}")
-    public Offer getOfferById(@PathVariable long id) {
+    public OfferDto getOfferById(@PathVariable long id) {
         return offerService.getById(id);
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseOfferDto addOffer(@RequestBody CreateOfferDto offerDto) {
+    public OfferDto addOffer(@RequestBody CreateOfferDto offerDto) {
         return offerService.addOffer(offerDto);
+    }
+
+    @PutMapping("{id}")
+    public OfferDto updateOffer(@PathVariable long id, @RequestBody CreateOfferDto offerDto) {
+        return offerService.updateOffer(id, offerDto);
+    }
+
+    @DeleteMapping("{id}")
+    public OfferDto deleteOffer(@PathVariable long id) {
+        return offerService.deleteOffer(id);
+    }
+
+    @PostMapping("feature")
+    public ResponseOfferFeatureDto addFeatureToOffer(@RequestParam long featureId, @RequestParam long offerId) {
+        return offerService.addFeatureToOffer(featureId, offerId);
     }
 }
