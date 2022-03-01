356406427cd814462f5aa7f0292eb8cf6a1cd771
Atalkais <krystiankalinowski8@gmail.com>
2022-03-01 10:17:24 +0100

    extend Feature

---
+++ src/main/java/dk/bec/polonez/reservationsystem/dto/offerDto/UpdateFeatureDto.java
@@ -0,0 +1,13 @@
+package dk.bec.polonez.reservationsystem.dto.offerDto;
+
+import lombok.Data;
+
+@Data
+public class UpdateFeatureDto {
+
+    private long id;
+
+    private String name;
+
+    private String description;
+}
