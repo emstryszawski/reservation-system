2194b1724c406889a88719ab0d8a434ab8c281c6
Atalkais <krystiankalinowski8@gmail.com>
2022-03-02 13:25:46 +0100

    featureDto refactor

--- src/main/java/dk/bec/polonez/reservationsystem/controller/FeatureController.java
+++ src/main/java/dk/bec/polonez/reservationsystem/controller/FeatureController.java
@@ -1,7 +1,7 @@
 package dk.bec.polonez.reservationsystem.controller;
 
 import dk.bec.polonez.reservationsystem.dto.feature.CreateFeatureDto;
-import dk.bec.polonez.reservationsystem.dto.feature.FeatureDto;
+import dk.bec.polonez.reservationsystem.dto.feature.ResponseFeatureDto;
 import dk.bec.polonez.reservationsystem.service.FeatureService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +18,30 @@ public class FeatureController {
     }
 
     @GetMapping
-    public List<FeatureDto> getAllFeatures() {
+    public List<ResponseFeatureDto> getAllFeatures() {
         return featureService.getAll();
     }
 
     @GetMapping("{id}")
-    public FeatureDto getFeatureById(@PathVariable long id) {
+    public ResponseFeatureDto getFeatureById(@PathVariable long id) {
         return featureService.getFeatureById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public FeatureDto addFeature(@RequestBody CreateFeatureDto featureDto) {
+    public ResponseFeatureDto addFeature(@RequestBody CreateFeatureDto featureDto) {
         return featureService.addFeature(featureDto);
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public FeatureDto updateFeature(@RequestBody CreateFeatureDto featureDto, @PathVariable long id) {
+    public ResponseFeatureDto updateFeature(@RequestBody CreateFeatureDto featureDto, @PathVariable long id) {
         return featureService.updateFeature(featureDto, id);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.GONE)
-    public FeatureDto deleteFeatureById(@PathVariable long id) {
+    public ResponseFeatureDto deleteFeatureById(@PathVariable long id) {
         return featureService.deleteFeatureById(id);
     }
 }
