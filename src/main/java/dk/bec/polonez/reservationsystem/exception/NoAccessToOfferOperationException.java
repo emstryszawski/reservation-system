c2c9e18d38c83f2925ef046f2c117b96e3e48fbe
est <est@bec.dk>
2022-03-03 14:24:47 +0100

    exceptions : final version

--- src/main/java/dk/bec/polonez/reservationsystem/exception/NoAccessToOfferOperationException.java
+++ src/main/java/dk/bec/polonez/reservationsystem/exception/NoAccessToOfferOperationException.java
@@ -5,4 +5,8 @@ public class NoAccessToOfferOperationException extends RuntimeException {
     public NoAccessToOfferOperationException(String message) {
         super(message);
     }
+
+    public NoAccessToOfferOperationException() {
+        super("Access denied, log as Place Owner or System Admin");
+    }
 }
