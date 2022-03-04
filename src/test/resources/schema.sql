6b53ffe35bef5e226b32790f18692d3c8b698624
est <est@bec.dk>
2022-03-04 14:03:08 +0100

    replaced schema.sql in test resources

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,23 +1,41 @@
-INSERT INTO ROLE(ID, ADD_FEATURES,	CD_PLACE,	CRUD_PLACE,	CRUD_REQUESTS,
-	CRUD_RESERVATION,	CRUD_USERS,	NAME,	READ_MY_RESERVATIONS)
-VALUES(1, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, 'USER', TRUE),
-        (2, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, 'PLACE_OWNER', TRUE),
-        (3, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 'SYS_ADMIN', TRUE);
+INSERT INTO ROLE(
+    ID,
+    NAME,
+    HAS_OFFER_CREATE_PRIVILEGE,
+    HAS_OFFER_READ_PRIVILEGE,
+    HAS_OFFER_UPDATE_PRIVILEGE,
+    HAS_OFFER_DELETE_PRIVILEGE,
+    HAS_OFFER_UPDATE_OTHERS_OFFER_PRIVILEGE,
+    HAS_OFFER_DELETE_OTHERS_OFFER_PRIVILEGE,
 
-INSERT INTO USER(ID, NAME, ROLE_ID, BLOCKED) VALUES(1001, 'PC1', 2, FALSE);
-INSERT INTO USER(ID, NAME, ROLE_ID, BLOCKED) VALUES(1004, 'PC2', 2, FALSE);
-INSERT INTO USER(ID, NAME, ROLE_ID, BLOCKED) VALUES(1002, 'SA1', 3, FALSE);
-INSERT INTO USER(ID, NAME, ROLE_ID, BLOCKED) VALUES(1003, 'USR1', 1, FALSE);
+    HAS_RESERVATION_MINE_ONLY_PRIVILEGE,
+    HAS_RESERVATION_READ_PRIVILEGE,
+    HAS_RESERVATION_CREATE_PRIVILEGE,
+    HAS_RESERVATION_DELETE_PRIVILEGE,
+    HAS_RESERVATION_UPDATE_PRIVILEGE,
+    HAS_RESERVATION_MY_OFFERS_PRIVILEGE,
+    HAS_RESERVATION_CANCEL_PRIVILEGE,
 
---INSERT INTO OFFER(ID, DESCRIPTION, NAME, OWNER_ID)
---VALUES
---    (1, 'OFERERERE', 'HOTEL', 1001),
---    (2, '7D67SDF', 'RESTURACJA', 1002);
-
-INSERT INTO FEATURE(ID,NAME,DESCRIPTION)
+    HAS_DISCOUNT_CREATION_PRIVILEGE,
+    HAS_DISCOUNT_READ_PRIVILEGE,
+    HAS_DISCOUNT_DELETE_PRIVILEGE,
+    HAS_DISCOUNT_UPDATE_PRIVILEGE
+    )
 VALUES
+    (1, 'USER',        false, true,  false, false, false, false,
+                       true,  true,  true,  false, true,  true,  false,
+                       false, false, false, true),
+    (2, 'PLACE_OWNER', true,  true,  true,  true,  false, false,
+                       false, true,  false, false, false, true,  true,
+                       true,  true,  true,  true),
+    (3, 'SYS_ADMIN',   true,  true,  true,  true,  true,  true,
+                       false, true,  false, true,  true,  false, false,
+                       true,  true,  true,  true);
+INSERT INTO FEATURE(
+    ID,
+    NAME,
+    DESCRIPTION
+)   VALUES
     (1,'Testowy 1','Opis 1'),
     (2,'Testowy 2','Opis 2'),
     (3,'Testowy 3','Opis 3');
--- SELECT * FROM USER U
--- JOIN ROLE R ON U.ROLE_ID = R.ID
